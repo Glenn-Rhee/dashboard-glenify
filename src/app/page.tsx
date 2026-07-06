b27b382263a899dff744f469d8a659a28957fd38
Glenn-Rhee <glennviktor5@gmail.com>
2026-07-06 15:05:36 +0700

    refactor: refactoring home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,20 @@ import data from "./data.json";
 import { TableRequestArtist } from "@/components/TableRequestArtist";
 import z from "zod";
 import { schemaTableReqArtist } from "@/components/table/columns-request-artist";
+import Container from "@/components/Container";
 
 export default function Home() {
   return (
-    <div className="flex flex-1 flex-col">
-      <div className="@container/main flex flex-1 flex-col gap-2">
-        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-          <SectionCards />
-          <div className="px-4 lg:px-6">
-            <ChartAreaInteractive />
-          </div>
-          <div className="px-4 lg:px-6">
-            <TableRequestArtist data={data as z.infer<typeof schemaTableReqArtist>[]} />
-          </div>
-        </div>
+    <Container>
+      <SectionCards />
+      <div className="px-4 lg:px-6">
+        <ChartAreaInteractive />
       </div>
-    </div>
+      <div className="px-4 lg:px-6">
+        <TableRequestArtist
+          data={data as z.infer<typeof schemaTableReqArtist>[]}
+        />
+      </div>
+    </Container>
   );
 }
---
+++ src/components/Container.tsx
@@ -0,0 +1,15 @@
+interface ContainerProps {
+  children: React.ReactNode;
+}
+export default function Container(props: ContainerProps) {
+  const { children } = props;
+  return (
+    <div className="flex flex-1 flex-col">
+      <div className="@container/main flex flex-1 flex-col gap-2">
+        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
