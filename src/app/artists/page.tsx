b43f3e2d3c656ecbef758b74e04cd00cfaf4cd61
Glenn-Rhee <glennviktor5@gmail.com>
2026-07-06 15:24:31 +0700

    refactor: refactoring tabs

--- src/app/artists/page.tsx
+++ src/app/artists/page.tsx
@@ -1,7 +1,17 @@
 import Container from "@/components/Container";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function ArtistsPage() {
-    return <Container>
-        cihu
+  return (
+    <Container>
+      <Tabs defaultValue="artist-list">
+        <TabsList>
+          <TabsTrigger value="artist-list">Aritst List</TabsTrigger>
+          <TabsTrigger value="artist-statistics">Aritst Statistics</TabsTrigger>
+        </TabsList>
+        <TabsContent value="artist-list">cihuy</TabsContent>
+        <TabsContent value="artist-statistics">cihuy 2</TabsContent>
+      </Tabs>
     </Container>
+  );
 }
