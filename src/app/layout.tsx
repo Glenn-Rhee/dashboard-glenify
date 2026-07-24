e4064c0cc86f0040a416957c62f6023ab12bae7e
Ariel Rizki Muhtamad Bakri <135257091+Glenn-Rhee@users.noreply.github.com>
2026-07-24 21:20:21 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Glenify-Dashboard",
-  description: "G",
+  description: "Gl",
 };
 
 export default function RootLayout({
