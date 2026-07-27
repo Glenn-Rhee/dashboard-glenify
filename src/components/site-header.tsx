4d22ecd53829a6bd05d40c94d4d6396ec80ab080
Glenn-Rhee <glennviktor5@gmail.com>
2026-07-27 16:05:05 +0700

    refactor: refactoring header path

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,32 +1,27 @@
 "use client";
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { pathRegister } from "@/utils/headerPath";
 import { usePathname } from "next/navigation";
-const pathResgiter: Map<string, string> = new Map([
-  ["/", "Dashboard"],
-  ["/artists", "Moderation Artists"],
-  ["/songs", "Moderation Songs"],
-  ["/albums", "Moderation Albums"],
-  ["/statistics", "Statistics"],
-  ["/playlists", "Playlists"],
-  ["/users", "Users"],
-  ["/transactions", "Transactions"],
-]);
 
 export function SiteHeader() {
   const pathname = usePathname();
-  const path = pathResgiter.get(pathname);
+  const path = pathRegister.get(pathname);
   if (!path) return null;
 
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
-      <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
-        <SidebarTrigger className="-ml-1" />
-        <Separator
-          orientation="vertical"
-          className="mx-2 data-[orientation=vertical]:h-4"
-        />
-        <h1 className="text-base font-medium">{path}</h1>
+      <div className="w-full flex justify-between px-4 lg:gap-2 lg:px-6">
+        <div className="flex w-full items-center gap-1 ">
+          <SidebarTrigger className="-ml-1" />
+          <Separator
+            orientation="vertical"
+            className="mx-2 data-[orientation=vertical]:h-4"
+          />
+          <h1 className="text-base font-medium">{path.title}</h1>
+        </div>
+
+        {path.action}
       </div>
     </header>
   );
---
+++ src/utils/headerPath.tsx
@@ -0,0 +1,70 @@
+import { UserRoundPlus } from "lucide-react";
+import Link from "next/link";
+import { JSX } from "react/jsx-runtime";
+
+interface ValueRegister {
+  title: string;
+  action: null | JSX.Element;
+}
+export const pathRegister: Map<string, ValueRegister> = new Map([
+  [
+    "/",
+    {
+      title: "Dashboard",
+      action: null,
+    },
+  ],
+  [
+    "/artists",
+    {
+      title: "Moderation Artists",
+      action: (
+        <Link href={"/artists/request"} className="cursor-pointer">
+          <UserRoundPlus className="size-5" />
+        </Link>
+      ),
+    },
+  ],
+  [
+    "/songs",
+    {
+      title: "Moderation Songs",
+      action: null,
+    },
+  ],
+  [
+    "/albums",
+    {
+      title: "Moderation Albums",
+      action: null,
+    },
+  ],
+  [
+    "/statistics",
+    {
+      title: "Statistics",
+      action: null,
+    },
+  ],
+  [
+    "/playlists",
+    {
+      title: "Playlists",
+      action: null,
+    },
+  ],
+  [
+    "/users",
+    {
+      title: "Users",
+      action: null,
+    },
+  ],
+  [
+    "/transactions",
+    {
+      title: "Transactions",
+      action: null,
+    },
+  ],
+]);
