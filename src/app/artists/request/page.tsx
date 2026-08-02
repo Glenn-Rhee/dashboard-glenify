002d962d57148ab349f947affaefef0e4f256a53
Glenn-Rhee <glennviktor5@gmail.com>
2026-08-02 17:59:20 +0700

    feat: added request artist page

---
+++ src/app/artists/request/page.tsx
@@ -0,0 +1,9 @@
+import Container from "@/components/Container";
+
+export default function ArtistRequestPage() {
+    return (
+        <Container>
+            cihuy
+        </Container>
+    )
+};
