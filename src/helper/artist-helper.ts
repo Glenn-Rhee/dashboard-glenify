ba5ee79e5216136496f4a4f5c0bff451f1821260
Glenn-Rhee <glennviktor5@gmail.com>
2026-08-05 16:30:51 +0700

    feat: added helper for genre and status approval

--- src/helper/artist-helper.ts
+++ src/helper/artist-helper.ts
@@ -4,3 +4,7 @@ export const VERIFICATION_STATUS =
   FormSchema.filterArtist.shape.verificationStatus.unwrap().options;
 export const ACCOUNT_STATUS =
   FormSchema.filterArtist.shape.accountStatus.unwrap().options;
+export const GENRE_ARTIST =
+  FormSchema.filterReqArtist.shape.genre.unwrap().options;
+export const STATUS_APPROVAL_ARTIST =
+  FormSchema.filterReqArtist.shape.status.unwrap().options;
