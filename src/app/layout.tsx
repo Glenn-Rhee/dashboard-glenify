72ea7eae490ddfac4219ed3360b30319bef0b4c0
Glenn-Rhee <glennviktor5@gmail.com>
2026-08-07 20:20:14 +0700

    refactor: update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Glenify-Dashboard",
-  description: "Glenify Dashboard is a websi",
+  description: "Glenify Dashboard is a websit",
 };
 
 export default function RootLayout({
