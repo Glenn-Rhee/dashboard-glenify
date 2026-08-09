9c58b992b0924dccf3031f66f4683a8716e3d58f
Glenn-Rhee <glennviktor5@gmail.com>
2026-08-09 14:17:59 +0700

    feat: added tabs trigger at songs page

--- src/app/songs/page.tsx
+++ src/app/songs/page.tsx
@@ -1,3 +1,17 @@
+import Container from "@/components/Container";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 export default function SongsPage() {
-    return <h1>Songs page</h1>
-};
+  return (
+    <Container>
+      <Tabs defaultValue="song-list">
+        <TabsList variant={"line"}>
+          <TabsTrigger value="song-list">Songs List</TabsTrigger>
+          <TabsTrigger value="song-statistics">Song Statistics</TabsTrigger>
+        </TabsList>
+        <TabsContent value="song-list">cihuy</TabsContent>
+        <TabsContent value="song-statistics">cihuy 2</TabsContent>
+      </Tabs>
+    </Container>
+  );
+}
